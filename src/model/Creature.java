e1692de0df7a7aea53ac31857c7dc6667c81470e
QuantumBorg <strugackofil@gmail.com>
2018-07-27 22:07:18 +0300

    TraitsEnum -> Trait Typo fix

--- src/model/Creature.java
+++ src/model/Creature.java
@@ -3,12 +3,42 @@ package model;
 import java.util.ArrayList;
 
 /****************************
- * Класс, отвечающий за существо
+ * responsible for the creature
  * **************************/
 
 public class Creature {
 
-    //Свойства существа -> в порядке их получения
+    int totalHunger = 1;
+    int fatCapacity = 0;
+    int fatSize = 0;
+
+    boolean isPredator = false;
+    boolean isBig = false;
+    boolean isRunning = false;
+    boolean isMimetic = false;
+    boolean isGrazing = false;
+    boolean isPoisonous = false;
+    boolean isTailLossable = false;
+    boolean isHibernatable = false;
+    boolean isScavenger = false;
+    boolean isPirate = false;
+    boolean isBurrowing = false;
+    boolean isCamouflaged = false;
+    boolean isSharp = false;
+    boolean isInfected = false;
+    boolean isSwimming = false;
+
+    ArrayList<Creature> communicationList = new ArrayList<>();
+    ArrayList<Creature> cooperationList = new ArrayList<>();
+
+    //crocodile: this creature can not eat if any symbiont is hungry; this creature can not be eaten if this animal is alive
+    ArrayList<Creature> symbiontList = new ArrayList<>();
+
+    //birds: can not eat if this creature is hungry; can not be eaten if this animal is alive
+    ArrayList<Creature> otherAnimalList = new ArrayList<>();
+
+
+    //Creature`s traits list (in order of obtaining)
     ArrayList<Trait> traits = new ArrayList<>();
 
     Creature(){
@@ -25,12 +55,13 @@ public class Creature {
 
 
     //Добавить свойство
-    void addTrait(Trait trait){
+    boolean addTrait(Trait trait){
         for(Trait t : traits) {
-            if(t.equals(trait)) return;
+            if(t.equals(trait))
+                return false;
         }
-
         traits.add(trait);
+        return true;
     }
     //Удалить свойство
     void deleteTrait(Trait trait){
