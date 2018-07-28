414fc81c22049ac5f89e141a07de24e2326db7c2
QuantumBorg <strugackofil@gmail.com>
2018-07-28 15:14:54 +0300

    isEmpty

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,9 +1,15 @@
 package model;
 
 public class Table {
-
-    int fodder = 0;
+   int fodder = 0;
 
     Table(){}
 
+
+    boolean isEmpty()
+    {
+        if (fodder == 0)
+            return true;
+        return false;
+    }
 }
