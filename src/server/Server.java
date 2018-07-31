42cc85485bd671f1f4f346a93d5d82d2a5e46150
anton <kropotin.anton@yandex.ru>
2018-07-31 13:07:21 +0300

    Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -38,7 +38,7 @@ public class Server {
                 new PlayerListener(Thread.currentThread(), newPlayer, table).start();
 
             } catch (IOException e) {
-                System.out.println("Connect with player fall");
+                System.out.println("Server: Connect with player fall");
             }
 
         }
@@ -46,26 +46,95 @@ public class Server {
 
     public boolean middlePlay(){ // Забавное название ?
 
+        boolean end = false;
         while (true){
 
-            for(int i = 0; i < 4; ++i){ // Фазы
+            for(int phase = 0; phase < 4; ++phase){ // Фазы
 
-                for(Player player : table.getPlayers()){ // Игроки
+                switch (phase) {
+                    case 0:
 
-                    //TODO: что-то им отправляем -> какие действия мы от игрока ждем
+                        ///region GROWTH
+                        for (Player player : table.getPlayers()) { // Игроки
 
-                    try {
-                        wait(); // Ждем пока не получим ответ
-                    } catch (InterruptedException e) {
-                        System.out.println("Server: problems with wait");
-                    }
+                            //TODO: что-то им отправляем -> какие действия мы от игрока ждем
 
-                    // Массовая рассылка результата
+                            try {
+                                wait(); // Ждем пока не получим ответ
+                            } catch (InterruptedException e) {
+                                System.out.println("Server: Problems with wait");
+                            }
+
+                            // Массовая рассылка результата
+                        }
+
+                        break;
+                        ///endregion
+
+                    case 1:
+
+                        ///region CALC_FODDER_BASE
+                        for (Player player : table.getPlayers()) { // Игроки
+
+                            //TODO: что-то им отправляем -> какие действия мы от игрока ждем
+
+                            try {
+                                wait(); // Ждем пока не получим ответ
+                            } catch (InterruptedException e) {
+                                System.out.println("Server: Problems with wait");
+                            }
+
+                            // Массовая рассылка результата
+                        }
+
+                        break;
+                        ///endregion
+
+                    case 2:
+
+                        ///region EATING
+                        for (Player player : table.getPlayers()) { // Игроки
+
+                            //TODO: что-то им отправляем -> какие действия мы от игрока ждем
+
+                            try {
+                                wait(); // Ждем пока не получим ответ
+                            } catch (InterruptedException e) {
+                                System.out.println("Server: Problems with wait");
+                            }
+
+                            // Массовая рассылка результата
+                        }
+
+                        break;
+                        ///endregion
+
+                    case 3:
+
+                        ///region EXTINCTION
+                        for (Player player : table.getPlayers()) { // Игроки
+
+                            //TODO: что-то им отправляем -> какие действия мы от игрока ждем
+
+                            try {
+                                wait(); // Ждем пока не получим ответ
+                            } catch (InterruptedException e) {
+                                System.out.println("Server: Problems with wait");
+                            }
+
+                            // Массовая рассылка результата
+                        }
+
+                        break;
+                        ///endregion
+
+                        // Здесь же будет и раздача карт
                 }
 
+                //Вероятно, рассылка результата бедет здесь
             }
 
-
+            if(end) return true;
         }
 
         return false;
