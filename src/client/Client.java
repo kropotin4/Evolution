bf7f165f8475eefb718da26ce8319a1f0955158c
anton <kropotin.anton@yandex.ru>
2018-08-13 13:12:19 +0300

    Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,27 @@
 package client;
 
-public class Client {
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client extends Thread{
+
+    String ip;
+    int port;
+
+    String login;
+
+    ServerListener serverListener;
+
+    Client(String login) throws IOException {
+
+        Socket server = new Socket(ip, port);
+
+        serverListener = new ServerListener(server, this);
+        serverListener.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- src/client/ServerListener.java
+++ src/client/ServerListener.java
@@ -1,7 +1,56 @@
 package client;
 
+import server.message.Message;
+import server.message.StartMessage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ServerListener extends Thread {
 
+    Socket server;
+    Client client;
+
+    ObjectOutputStream os;
+    ObjectInputStream is;
+
+    ServerListener(Socket server, Client client) throws IOException {
+        this.server = server;
+
+        os = new ObjectOutputStream(server.getOutputStream());
+        is = new ObjectInputStream(server.getInputStream());
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            os.writeObject(new StartMessage(client.login));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
+        Object mesObject = null;
+        while(true){
+
+            try {
+                mesObject = is.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                //TODO: что-то сделать
+                continue;
+            }
+
+            if(mesObject instanceof Message){
+
+                Message message = (Message) mesObject;
+
+
+
+            }
+        }
+
+    }
 }
