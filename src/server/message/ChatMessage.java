c1ff5cbb5a97772aa379b1fa53be9b98da17ba18
kropotin4 <kropotin.anton@yandex.ru>
2018-12-31 22:59:07 +0300

    Доработка чата.

--- src/server/message/ChatMessage.java
+++ src/server/message/ChatMessage.java
@@ -2,8 +2,11 @@ package server.message;
 
 public class ChatMessage extends Message {
 
+    final String login;
+
     public ChatMessage(String login, String message){
         super(MessageType.CHAT);
+        this.login = login;
         setMes("\"" + login + "\": " + message);
     }
 
--- src/view/gui/Chat.java
+++ src/view/gui/Chat.java
@@ -24,6 +24,7 @@ public class Chat extends VBox {
                     "   * {\n" +
                     "    margin: 0;\n" +
                     "    padding: 0;\n" +
+                    "    word-wrap: break-word;\n" +
                     "   }\n" +
             "</style>" +
             "</head><body contentEditable=\"false\">";
